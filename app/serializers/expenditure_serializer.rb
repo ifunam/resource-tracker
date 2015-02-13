5075f9f8ad560c1518a62c0942fe2c7c7ec2ed82
Developer at IFUNAM <dev@fisica.unam.mx>
2015-02-13 15:37:29 -0600

    The ActionView::Helpers::NumberHelper module was added into the ExpenditureSerializer

--- app/serializers/expenditure_serializer.rb
+++ app/serializers/expenditure_serializer.rb
@@ -1,3 +1,10 @@
 class ExpenditureSerializer < ActiveModel::Serializer
+  include ActionView::Helpers::NumberHelper
+  include ApplicationHelper
+
   attributes :key, :date, :motive, :amount, :condition
+
+  def amount
+    number_with_precision object.amount
+  end
 end
