e503824c0511d279a0ecb5eef2ea3880143f67ef
Developer at IFUNAM <dev@fisica.unam.mx>
2015-02-17 09:56:18 -0600

    The meta option was added to send the pagination info to the client from the index action. Also, not used code was removed

--- rails/app/controllers/api/v1/projects_controller.rb
+++ rails/app/controllers/api/v1/projects_controller.rb
@@ -3,22 +3,23 @@ class Api::V1::ProjectsController < ApplicationController
 
   # GET /projects
   def index
-    @projects = Project.all.page params[:page] || 1
-    render json: @projects
+    @projects = Project.all.page(params[:page]||1)
+    render json: @projects,
+           meta: {
+                current_page: @projects.current_page,
+                    per_page: @projects.default_per_page,
+                 total_pages: @projects.total_pages
+           }
   end
 
   # GET /projects/1
   def show
+    render json: @project
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_project
-      @project = Project.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def project_params
-      params[:project]
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_project
+    @project = Project.find(params[:id])
+  end
 end
