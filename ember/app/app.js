0f9905bfa8926c9f7984183708ea1089920eff8f
Developer at IFUNAM <dev@fisica.unam.mx>
2015-02-17 14:09:42 -0600

    First nested controller/models implementation for projects and lines

--- ember/app/app.js
+++ ember/app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
@@ -8,7 +9,8 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  ApplicationSerializer: DS.ActiveModelSerializer.extend({})
 });
 
 loadInitializers(App, config.modulePrefix);
--- ember/app/models/line.js
+++ ember/app/models/line.js
@@ -2,15 +2,10 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   key: DS.attr('string'),
-  financing_source: DS.attr('string'),
   name: DS.attr('string'),
-  sponsor: DS.attr('string'),
-  start_date: DS.attr('date'),
-  end_date: DS.attr('date'),
-  authorized_budget: DS.attr('string'),
   deposited_budget: DS.attr('string'),
   committed_budget: DS.attr('string'),
   spent_budget: DS.attr('string'),
 
-  lines: DS.hasMany('line', {async: true})
+  project: DS.belongsTo('project'),
 });
