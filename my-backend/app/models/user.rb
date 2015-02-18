4668b15fa9d2c69194bb4fce652560f3e691198c
Developer at IFUNAM <dev@fisica.unam.mx>
2015-02-18 16:27:46 -0600

    The shared_key attribute was added into the User model

--- my-backend/app/models/user.rb
+++ my-backend/app/models/user.rb
@@ -9,6 +9,7 @@ class User
   field :login,              type: String, default: ""
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
+  field :shared_key,         type: String
 
   ## Recoverable
   #field :reset_password_token,   type: String
