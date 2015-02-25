8104d997fdae30cc92606ab8b97b95a03a92ec7f
Developer at IFUNAM <dev@fisica.unam.mx>
2015-02-25 15:18:00 -0600

    Spanglish error fixed

--- my-backend/app/controllers/api/v1/data_migrations_controller.rb
+++ my-backend/app/controllers/api/v1/data_migrations_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DataMigrationsController < ApplicationController
     @data_migration = DataMigration.new(:backup => params[:backup])
     @data_migration.ip_address = request.remote_ip
     if @data_migration.save
-      render json: { msg: 'The data was succesfully saved' }, status: :ok
+      render json: { msg: 'The data were succesfully saved' }, status: :ok
     else
       render json: { msg: @data_migration.errors.full_messages }, status: 422
     end
