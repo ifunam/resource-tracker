e5cd37ff75cfac2a96edc9cf0867bcc5553726cb
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-09 16:34:02 -0600

    The authentication based on tokens was added into the API::V1::DataMigrations controller

--- backend/app/controllers/api/v1/data_migrations_controller.rb
+++ backend/app/controllers/api/v1/data_migrations_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::DataMigrationsController < ApplicationController
+  before_filter :authenticate_by_token!
+
   def create
     @data_migration = DataMigration.new(backup: params[:backup])
     @data_migration.ip_address = request.remote_ip
@@ -7,5 +9,14 @@ class Api::V1::DataMigrationsController < ApplicationController
     else
       render json: { msg: @data_migration.errors.full_messages }, status: 422
     end
+  end
+
+  private
+
+  def authenticate_by_token!
+    secret_path = File.join(Rails.root, 'config/secrets.yml')
+    secret = IO.read(secret_path)
+    api_key = YAML.load(secret)[Rails.env]['api_key']
+    head :unauthorized unless api_key == request.headers.env['HTTP_X_API_TOKEN']
   end
 end
