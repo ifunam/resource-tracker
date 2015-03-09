06f24ed829bdbc54a101d12db2b498a5695cbfb3
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-09 11:22:31 -0600

    Ruby syntax updated

--- backend/app/controllers/api/v1/data_migrations_controller.rb
+++ backend/app/controllers/api/v1/data_migrations_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::DataMigrationsController < ApplicationController
   def create
-    @data_migration = DataMigration.new(:backup => params[:backup])
+    @data_migration = DataMigration.new(backup: params[:backup])
     @data_migration.ip_address = request.remote_ip
     if @data_migration.save
       render json: { msg: 'The data were succesfully saved' }, status: :ok
