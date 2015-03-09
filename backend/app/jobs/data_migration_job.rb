0ccf5fc52f5fa25a6a7b5b55eb9abdc5f56eccb7
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-09 11:06:37 -0600

    The DataMirationJob class was refactored

--- backend/app/jobs/data_migration_job.rb
+++ backend/app/jobs/data_migration_job.rb
@@ -1,57 +1,55 @@
 require 'zlib'
 require 'json'
+
 class DataMigrationJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
-    backup_path = args.first
-    if File.exist? backup_path
-      destroy_all
-      JSON.parse(Zlib::GzipReader.open(backup_path).read)['projects'].each do |p|
-        if p.has_key? 'user'
-          user = p['user']
-          p.delete 'user'
-        end
-
-        if p.has_key? 'lines'
-          lines = p['lines']
-          p.delete 'lines'
-        end
-
-        @project = Project.new(p)
-        @project.user = set_user(user)
-        @project.save if @project.valid?
-
-        lines.each do |l|
-          expenditures = l['expenditures']
-          l.delete 'expenditures'
-
-          @line = Line.new(l)
-          expenditures.each do |e|
-            @line.expenditures << Expenditure.new(e)
-          end
-
-          @project.lines << @line
-        end
-        @project.save
-
-      end
+  def perform(backup_path)
+    return unless File.exist? backup_path
+    destroy_all
+    @json = Zlib::GzipReader.open(backup_path).read
+    JSON.parse(@json)['projects'].each do |p|
+      import_project(p)
     end
   end
 
   private
+
   def destroy_all
-    [Expenditure, Line, Project].each do |klass|
-      klass.destroy_all
+    [Expenditure, Line, Project].collect(&:destroy_all)
+  end
+
+  def import_user(u)
+    if User.where(login: u['login']).exists?
+      User.where(login: u['login']).first
+    else
+      User.create(u.merge!(password: u['key'], password_confirmation: u['key']))
     end
   end
 
-  def set_user(h)
-    if User.where(login: h['login']).exists?
-      User.where(login: h['login']).first
-    else
-      h.merge!(password: h['key'], password_confirmation: h['key'])
-      User.create(h)
+  def import_lines(lines)
+    lines.collect do |l|
+      expenditures = l['expenditures']
+      l.delete 'expenditures'
+      @line = Line.new(l)
+      expenditures.each do |e|
+        @line.expenditures << Expenditure.new(e)
+      end
+      @line
     end
+  end
+
+  def import_project(p)
+    @user = import_user(p['user'])
+    p.delete_if { |k, v| v if k == 'user' }
+
+    @lines = import_lines(p['lines'])
+    p.delete_if { |k, v| v if k == 'lines' }
+
+    @project = Project.new(p)
+    @project.user = @user
+    @project.save # Save it before to avoid troubles with its childrens
+    @project.lines << @lines
+    @project.save
   end
 end
