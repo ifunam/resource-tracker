fbc5c94c89fb1eb9dfa723c2c7ff343217cd5c9a
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-10 17:56:21 -0600

    Enabling symbolic links for tmp and log directories for the deployment script with Capistrano

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -30,7 +30,7 @@ set :repo_tree, 'backend'
 set :linked_files, fetch(:linked_files, []).push('config/mongoid.yml', 'config/secrets.yml', 'config/ldap.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
