7974b299bc18050707e25d281d448b6b251cffd0
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-09 18:17:50 -0600

    The protect_from_forgery was enabled with null_session support in the ApplicationController clas

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
-  #
-  before_filter :authenticate_user_from_token!
+  protect_from_forgery with: :null_session
 
-  #protect_from_forgery with: :null_session
+  before_filter :authenticate_user_from_token!
 
   private
 
