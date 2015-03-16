2a7aa0b7a5eb93da98d4f33a40180147035181c9
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-16 01:06:11 -0600

    minor change

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
- import config from '../config/environment';
+import config from '../config/environment';
+
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
   host: config.apiHost
