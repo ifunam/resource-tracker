0a27a71a412f7fc9d756b8c9615ebb560f867dbd
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-16 12:32:50 -0600

    The rack-cors gem was added to make cross-origin AJAX possible

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -35,6 +35,23 @@ module ResourceTracker
       g.test_framework :rspec, :fixture => true, :views => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
     config.active_job.queue_adapter = :delayed_job
+
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+        resource '/api',
+          :headers => :any,
+          :methods => [:post],
+          :credentials => true,
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 0
+      end
+    end
   end
 end
