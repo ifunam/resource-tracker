877fe96093824d709eee919ba6412a708397f3bf
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-16 12:46:14 -0600

    The crossdomain ajax was enabled for our RestAdapter

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -3,6 +3,12 @@ import config from '../config/environment';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
-  host: config.apiHost
+  host: config.apiHost,
+  ajax: function(url, method, hash) {
+    hash = hash || {}; // hash may be undefined
+    hash.crossDomain = true;
+    hash.xhrFields = {withCredentials: true};
+    return this._super(url, method, hash);
+  }
 });
 
