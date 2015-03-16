1ed2f1ed3a0c3a3573664731a8e092d477099dbc
Developer at IFUNAM <dev@fisica.unam.mx>
2015-03-16 00:04:42 -0600

    Fixing routes based in ember-auth-simple

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1'
-  // host: 'http://localhost:3000'
+  // host: 'https://presupuestos.fisica.unam.mx:443'
 });
 
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,14 +6,13 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('application');
+  this.route('login');
   this.resource('projects', function() {
     this.route('show', {path: ':project_id'});
     this.resource('lines', function() {
       this.route('show', {path: ':line_id'});
     });
   });
-  this.route('login');
 });
 
 export default Router;
