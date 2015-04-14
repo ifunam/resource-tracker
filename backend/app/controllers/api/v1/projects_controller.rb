0b6bafde860e679cfdd8c84e07f50e64de91a7cb
Developer at IFUNAM <dev@fisica.unam.mx>
2015-04-13 21:47:14 -0500

    The meta method was added to the Api::V1::ProjectsController class

--- backend/app/controllers/api/v1/projects_controller.rb
+++ backend/app/controllers/api/v1/projects_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::ProjectsController < Api::V1::BaseController
+  include ActionView::Helpers::NumberHelper
   before_action :set_project, only: [:show]
 
   # GET /projects
   def index
     @projects = current_api_v1_user.valid_projects.all.page(params[:page] || 1)
-    render json: @projects,
-      meta: {
-        current_page: @projects.current_page,
-        per_page: @projects.default_per_page,
-        total_pages: @projects.total_pages
-      }
+    render json: @projects, meta: meta
   end
 
   # GET /projects/1
@@ -21,5 +17,22 @@ class Api::V1::ProjectsController < Api::V1::BaseController
 
   def set_project
     @project = Project.find(params[:id])
+  end
+
+  def meta
+    {
+      pagination: {
+        current: @projects.current_page,
+        per: @projects.default_per_page,
+        total: @projects.total_pages
+      },
+      total: {
+        authorized_budget: number_with_precision(@projects.total_authorized_budget),
+        deposited_budget: number_with_precision(@projects.total_deposited_budget),
+        committed_budget: number_with_precision(@projects.total_committed_budget),
+        spent_budget: number_with_precision(@projects.total_spent_budget),
+        balance: number_with_precision(@projects.total_balance)
+      }
+    }
   end
 end
