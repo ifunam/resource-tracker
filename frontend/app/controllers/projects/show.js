4be3bef88e1e2533e4d6a10c6baae9fc4de5432e
Developer at IFUNAM <dev@fisica.unam.mx>
2015-04-21 12:02:47 -0500

    Removing commentted code

--- frontend/app/controllers/projects/show.js
+++ frontend/app/controllers/projects/show.js
@@ -3,9 +3,5 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   model: function(params) {
     return this.store.fetchById('project', params.slug);
-  },
-  //meta: function() {
-  //  console.log(this.store.metadataFor('line').meta);
-  //  return 'Hello';
-  //}.property()
+  }
 });
